a18bf1567f75c1002f2d8699530ef9bbf009852a
Ja1z666 <thatsameuser@icloud.com>
2023-09-12 21:45:51 +0300

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
-use colored::Colorize;
 use temp_mail::Email;
+
+const GRAY: &str = "\x1b[30;1m";
+const RESET: &str = "\x1b[0m";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut email = Email::new().await?;
     let mut count = 0;
 
-    println!("{}{}\n", email.address, "(already copied!)".cyan());
+    println!(
+        "{}{}\n",
+        email.address,
+        format!("{}(already copied!){}", GRAY, RESET)
+    );
 
     loop {
         email.get_messages().await?;
