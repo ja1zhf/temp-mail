64ae67d7b35ba1e60a6b13238fc0aaf5507af8ab
Ja1z666 <thatsameuser@icloud.com>
2023-09-13 00:25:00 +0300

    add install

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,1 +1,3 @@
-
+New-Item -Path 'C:\Program Files\tmail' -ItemType Directory
+Invoke-WebRequest "https://github.com/Ja1z666/temp-mail/releases/download/1.0.0/tmail.exe" -OutFile "C:\Program Files\tmail\tmail.exe"
+$env:Path += ';C:\Program Files\tmail'
