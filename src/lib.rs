5997c76bb9c7f87cb78e9bd978ce6764f53ba6b9
Teyllayka <teyylayt@gmail.com>
2023-09-13 20:10:53 +0300

    fix

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use clipboard::ClipboardContext;
 use clipboard::ClipboardProvider;
 use dialoguer::{theme::ColorfulTheme, Select};
+use scraper::{Html, Selector};
 use serde::Deserialize;
 
 mod colors;
@@ -48,12 +49,12 @@ pub struct Email {
 
 impl Email {
     pub async fn new() -> Result<Self, reqwest::Error> {
-        let request = reqwest::get(format!("{}?action=genRandomMailbox&count=1", API))
+        let response = reqwest::get(format!("{}?action=genRandomMailbox&count=1", API))
             .await?
             .text()
             .await?;
 
-        let data: GetEmail = serde_json::from_str(&request).unwrap();
+        let data: GetEmail = serde_json::from_str(&response).unwrap();
 
         let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
         ctx.set_contents(data.email.clone()).unwrap();
@@ -66,7 +67,7 @@ impl Email {
 
     pub async fn get_messages(&mut self) -> Result<(), reqwest::Error> {
         let auth: Vec<&str> = self.address.split('@').collect();
-        let request = reqwest::get(format!(
+        let response = reqwest::get(format!(
             "{}?action=getMessages&login={}&domain={}",
             API, auth[0], auth[1]
         ))
@@ -74,16 +75,14 @@ impl Email {
         .text()
         .await?;
 
-        let data: Vec<MessagePreview> = serde_json::from_str(&request).unwrap();
-
-        self.messages = data;
+        self.messages = serde_json::from_str(&response).unwrap();
 
         Ok(())
     }
 
     async fn get_message(&mut self, id: usize) -> Result<MessageContant, reqwest::Error> {
         let auth: Vec<&str> = self.address.split('@').collect();
-        let request = reqwest::get(format!(
+        let response = reqwest::get(format!(
             "{}?action=readMessage&login={}&domain={}&id={}",
             API, auth[0], auth[1], id
         ))
@@ -91,20 +90,22 @@ impl Email {
         .text()
         .await?;
 
-        let data: MessageContant = serde_json::from_str(&request).unwrap();
+        let data: MessageContant = serde_json::from_str(&response).unwrap();
 
         Ok(data)
     }
 
     pub async fn print_messages(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        let mut msgs: Vec<String> = Vec::new();
-
-        for message in &self.messages {
-            msgs.push(format!(
-                "From: {}\nSubject: {}\nDate: {}\n",
-                message.from, message.subject, message.date
-            ));
-        }
+        let mut msgs: Vec<String> = self
+            .messages
+            .iter()
+            .map(|message| {
+                format!(
+                    "From: {}\nSubject: {}\nDate: {}\n",
+                    message.from, message.subject, message.date
+                )
+            })
+            .collect();
 
         msgs.push("Reload".to_string());
 
@@ -117,10 +118,20 @@ impl Email {
         if selection != msgs.len() - 1 {
             let data = self.get_message(self.messages[selection].id).await?;
 
-            println!(
-                "From: {GRAY}{}{RESET} Subject: {GRAY}{}{RESET}, Date: {GRAY}{}{RESET}\n\n{}",
-                data.from, data.subject, data.date, data.textBody
-            );
+            if data.textBody.len() != 0 {
+                println!(
+                    "From: {GRAY}{}{RESET} Subject: {GRAY}{}{RESET}, Date: {GRAY}{}{RESET}\n\n{}",
+                    data.from, data.subject, data.date, data.textBody
+                );
+            } else {
+                let html = Html::parse_document(&data.htmlBody);
+                let selector = Selector::parse("a").unwrap();
+                for element in html.select(&selector) {
+                    let text = element.text().collect::<Vec<_>>();
+                    let hrefs = element.value().attr("href").unwrap();
+                    println!("{}: {}", text.join(" "), hrefs);
+                }
+            }
 
             Select::with_theme(&ColorfulTheme::default())
                 .default(0)
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use tmail::{Email, GRAY, RESET};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    winconsole::console::set_title("Temp Mail by ja1z + teyllay").unwrap();
+    print!("\x1B[2J\x1B[1;1H");
     let mut email = Email::new().await?;
 
     println!(
