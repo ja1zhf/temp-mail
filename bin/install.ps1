118361aa90d78d5b68d3d157560d6d8df2ceb317
Ja1z666 <thatsameuser@icloud.com>
2023-09-13 20:25:28 +0300

    update install

--- bin/install.ps1
+++ bin/install.ps1
@@ -1,5 +1,5 @@
 New-Item -Path 'C:\Program Files\tmail' -ItemType Directory
-Invoke-WebRequest "https://github.com/Ja1z666/temp-mail/releases/download/1.0.0/tmail.exe" -OutFile "C:\Program Files\tmail\tmail.exe"
+Invoke-WebRequest "https://github.com/Ja1z666/temp-mail/releases/download/1.0.1/tmail.exe" -OutFile "C:\Program Files\tmail\tmail.exe"
 $envPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
 $newPath = $envPath + ";C:\Program Files\tmail"
 [System.Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::User)
